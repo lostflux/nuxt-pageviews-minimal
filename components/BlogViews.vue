76bb516e52640f980700c2f6da76b84fbaded7a8
siavava <amittaijoel@outlook.com>
2024-01-02 14:08:14 -0500

    muted-views

---
+++ components/BlogViews.vue
@@ -0,0 +1,9 @@
+<!-- <script lang="ts" setup>
+const views = await usePageViews()
+</script>
+
+<template>
+  <div>
+    <div>Blog views: {{ views }}</div>
+  </div>
+</template> -->
---
+++ pages/[...slug].vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <!-- <BlogViews /> -->
+    <ContentDoc />
+  </main>
+</template>
