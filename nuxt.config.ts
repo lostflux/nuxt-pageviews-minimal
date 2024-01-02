90810f76d9e51d1db92c20801394aaca29be194e
siavava <amittaijoel@outlook.com>
2024-01-02 18:24:19 -0500

    test fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  hooks: {
+    "close": () => {
+      process.exit(0)
+    }
+  },
   modules: [
     '@nuxt/content',
     'nuxt-pageviews'
